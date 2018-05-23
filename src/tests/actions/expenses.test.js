71c8f44e7c5f9c439b4af8e7763555e1ceefa301
elwin <polozero1@yahoo.com.tw>
2018-05-23 23:18:49 +0800

    finish on firebase auth

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,10 +1,20 @@
-import { addExpense, editExpense, removeExpense, startAddExpense } from '../../actions/expenses';
+import { addExpense, editExpense, startEditExpense, removeExpense, startRemoveExpense, startAddExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
 import expenses from '../fixtures/expenses'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import database from '../../firebase/firebase'
 
+const uid = 'testID'
+const defaultAuthState = { auth: { uid } }
 const createMockStore = configureMockStore([thunk])
+
+beforeEach((done) => {
+  const expensesData = {};
+  expenses.forEach(({ id, description, note, amount, createdAt }) => {
+    expensesData[id] = { description, note, amount, createdAt }
+  })
+  database.ref(`users/${uid}/expenses`).set(expensesData).then(() => done())
+})
 
 test('should setup remove expense action object', () => {
   const action = removeExpense({ id: '123abc' });
@@ -12,6 +22,24 @@ test('should setup remove expense action object', () => {
     type: 'REMOVE_EXPENSE',
     id: '123abc'
   });
+});
+
+test('should setup remove expenses from firebase', (done) => {
+  const store = createMockStore(defaultAuthState)
+  const id = expenses[1].id
+
+  store.dispatch(startRemoveExpense({ id })).then(() => {
+    const actions = store.getActions()
+    
+    expect(actions[0]).toEqual({
+      type: 'REMOVE_EXPENSE',
+      id
+    })
+    return database.ref(`users/${uid}/expenses/${id}`).once('value')
+  }).then((snapshot) => {
+    expect(snapshot.val()).toBeFalsy()
+    done()
+  })
 });
 
 test('should setup edit expense action object', () => {
@@ -23,6 +51,26 @@ test('should setup edit expense action object', () => {
       note: 'New note value'
     }
   });
+});
+
+test('should edit expenses from firebase', (done) => {
+  const store = createMockStore(defaultAuthState)
+  const id = expenses[1].id
+  const updates = { note: 'this is new note' }
+
+  store.dispatch(startEditExpense(id, updates)).then(() => {
+    const actions = store.getActions()
+
+    expect(actions[0]).toEqual({
+      type: 'EDIT_EXPENSE',
+      id,
+      updates
+    })
+    return database.ref(`users/${uid}/expenses/${id}`).once('value')
+  }).then((snapshot) => {
+    expect(snapshot.val().note).toBe(updates.note)
+    done()
+  })
 });
 
 test('should setup add expense action object with provided values', () => {
@@ -40,7 +88,7 @@ test('should setup add expense action object with provided values', () => {
 });
 
 test('should add expense to database and store', (done) => {
-  const store = createMockStore({})
+  const store = createMockStore(defaultAuthState)
   const expensesData =  {
     description: 'Mouse',
     note: 'upgrade',
@@ -56,7 +104,9 @@ test('should add expense to database and store', (done) => {
         ...expensesData
       }
     })
-    return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    // return a promise to avoid callback hell, it can force the next then function to wait
+    // until the return function is donesee section 15, lecture 154
+    return database.ref(`users/${uid}/expenses/${actions[0].expense.id}`).once('value');
   }).then((snapshot) => {
     expect(snapshot.val()).toEqual(expensesData)
     done()
@@ -64,7 +114,7 @@ test('should add expense to database and store', (done) => {
 })
 
 test('should add expense with default to database and store', (done) => {
-  const store = createMockStore({})
+  const store = createMockStore(defaultAuthState)
   const defaultData = {
     description: '',
     note: '',
@@ -81,23 +131,29 @@ test('should add expense with default to database and store', (done) => {
         ...defaultData
       }
     })
-    return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    return database.ref(`users/${uid}/expenses/${actions[0].expense.id}`).once('value');
   }).then((snapshot) => {
     expect(snapshot.val()).toEqual(defaultData)
     done()
   })
 })
 
-// test('should setup add expense action object with default values', () => {
-//   const action = addExpense();
-//   expect(action).toEqual({
-//     type: 'ADD_EXPENSE',
-//     expense: {
-//       id: expect.any(String),
-//       description: '',
-//       note: '',
-//       amount: 0,
-//       createdAt: 0
-//     }
-//   });
-// });
+test('should setup set expense action object with data', () => {
+  const action = setExpenses(expenses);
+  expect(action).toEqual({
+    type: 'SET_EXPENSES',
+    expenses
+  });
+});
+
+test('should fetch the expenses from firebase', () => {
+  const store = createMockStore(defaultAuthState)
+  store.dispatch(startSetExpenses()).then(() => {
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: 'SET_EXPENSES',
+      expenses
+    })
+    done()
+  })
+})
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
+// import { startLogOut } from '../../actions/auth';
 
 test('should render Header correctly', () => {
-  const wrapper = shallow(<Header />);
+  const wrapper = shallow(<Header startLogOut={() => { }}/>);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('should call startLogOut on button click', () => {
+  const startLogOut = jest.fn();
+  const wrapper = shallow(<Header startLogOut={startLogOut} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogOut).toHaveBeenCalled();
+})
